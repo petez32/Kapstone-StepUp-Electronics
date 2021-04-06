43f0e7ec5e5b3b770760864dcb75d132a826c03e
Cedrick Randolph <naseer4biz@gmail.com>
2021-04-06 18:30:38 -0400

    setup layout and styled carousel, used mock products

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,24 @@
+import {Carousel} from 'react-bootstrap'
+import {products} from '../products.json'
+import '../asset/home.css'
 function Home() {
-
+    return (
+        <div id="home">
+            <Carousel id="product-carousel">
+                {products.map((item) => (
+                    <Carousel.Item key={item.id}>
+                        <img
+                            src={item.image}
+                            alt={item.name}
+                        />
+                        <Carousel.Caption>
+                            <h3>{item.name}</h3>
+                        </Carousel.Caption>
+                </Carousel.Item>
+                ))}
+            </Carousel>
+        </div>
+    )
 }
 
 export default Home;
