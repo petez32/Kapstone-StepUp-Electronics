64524c47ec7c630b96e4a086ab33fed3c186f9fa
Cedrick Randolph <naseer4biz@gmail.com>
2021-04-08 11:31:31 -0400

    added boiler plate

--- src/views/AllProduct.js
+++ src/views/AllProduct.js
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from 'react'
 
-// const AllProduct = () => {
-//     return (
-//         <div>
+const AllProduct = () => {
+    return (
+        <div>
             
-//         </div>
-//     )
-// }
+        </div>
+    )
+}
 
-// export default AllProduct
+export default AllProduct
