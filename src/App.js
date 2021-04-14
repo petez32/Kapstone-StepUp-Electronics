e78a24a6761da371774e9c423bce4b63693cbbc4
petez32 <73840127+petez32@users.noreply.github.com>
2021-04-14 14:37:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Signin from "./views/Signin";
 import Signup from "./views/Signup";
 import Profile from "./views/profile";
 import { useStore } from "./store/store"
-import AllProduct from "./views/AllProduct";
+import AllProducts from "./views/AllProducts";
 import { Switch, Route } from "react-router-dom";
 import Header from "./component/Header.js";
 import "./asset/home.css";
@@ -19,10 +19,7 @@ function App() {
       <Toast />
       <Switch>
         <Route exact path="/" component={Home} />
-        (!user.token){<Route path="/signin" component={Signin} />}
-        {user.token &&<div>
-          {user.userName}
-          </div>}
+        <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
         <Route path="/AllProducts" component={AllProducts} />
         <Route path="/profile" component={Profile} />
