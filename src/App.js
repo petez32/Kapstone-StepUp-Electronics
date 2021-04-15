9438c38c2666e2b2ec5d55b40d7013cca05d567e
nick5150 <ndummett35@gmail.com>
2021-04-15 15:20:50 -0400

    clickable links

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Home from "./views/Home";
 import Signin from "./views/Signin";
 import Signup from "./views/Signup";
 import Profile from "./views/profile";
-import { useStore } from "./store/store"
+import { useStore } from "./store/store";
 import AllProducts from "./views/AllProducts";
 import { Switch, Route } from "react-router-dom";
 import Header from "./component/Header.js";
 import "./asset/home.css";
+import CellPhone from "./views/phones";
+import Television from "./views/Televisions";
 
 function App() {
-  const user = useStore((state) => state.user)
+  //const user = useStore((state) => state.user);
   return (
     <div className="App">
       <Header />
@@ -22,7 +24,9 @@ function App() {
         <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
         <Route path="/AllProducts" component={AllProducts} />
+        <Route path="/products/phones" component={CellPhone} />
         <Route path="/profile" component={Profile} />
+        <Route path="/products/Televisions" component={Television} />
       </Switch>
     </div>
   );
