f35d62ef041c20f89aad1c36c2691d4b7974884f
zbaxter1 <73849986+zbaxter1@users.noreply.github.com>
2021-04-16 15:26:39 -0400

    added profile function and imported

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,19 +1,58 @@
 import React from "react";
+import Avatar from "@material-ui/core"
+import TextField from "@material-ui/core"
+
+
+// change username,
+function username(){
+return(
+  <div>
+
+  </div>
+)
+}
+
+
+// change password. 
+function Password(){
+  
+}
+
+
+//change address.
+
+
+
+// Add user 
+
+
+// Delete User
+
+
+
+//Credit Amount
+
+
+
+// Recent Purchases 
+
+
+
+// Recently viewed
+
+
+
+
+
+
+
+
 
 // Profile-Profile Picture with upload button,
-// change username,
-// change password, change address.
-
-
-
-
-
-
-
 function profile(props) {
   return (
     <div>
-      <img src="" alt="profile" />
+      <Avatar alt="default avatar" src="/" />
       <input type="file" />
       <button>Upload</button>
     </div>
