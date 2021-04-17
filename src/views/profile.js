31f40157f2d060aadd3e794b53b18911028b6e31
zbaxter1 <73849986+zbaxter1@users.noreply.github.com>
2021-04-17 13:09:26 -0400

    Added forms for profile page (not functioning or styled yet)

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,62 +1,56 @@
 import React from "react";
-import Avatar from "@material-ui/core"
-import TextField from "@material-ui/core"
-
+import Avatar from "@material-ui/core/Avatar";
+import { TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 // change username,
-function username(){
-return(
-  <div>
+function Password() {}
 
-  </div>
-)
-}
+function UserName(props) {}
 
-
-// change password. 
-function Password(){
-  
-}
-
+// change password.
 
 //change address.
 
+// Add user
+
+function profile(props) {
+  return (
+    <>
+      <Avatar alt="default avatar" src="/" />
+      <input type="file" />
+      <Button>Upload</Button>
+
+      <TextField id="user-name" label="Choose Username" defaultValue="" />
+
+      <Button> Change Username </Button>
+      <TextField id="user-password" label="Choose Password" defaultValue="" />
+      <Button>Change Password</Button>
+      <TextField id="user-fullname" label="Full Name" defaultValue="" />
+      <TextField id="user-street" label="Street Address" defaultValue="" />
+      <TextField id="user-apt" label="Apt,suite" defaultValue="" />
+      <TextField id="user-city" label="City" defaultValue="" />
+      <TextField id="user-state" label="State" defaultValue="" />
+      <TextField id="user-zip-code" type="number" label="Zip Code" defaultValue="" />
+      <Button>Change Address</Button>
+      
+      <h3>Recent purchases</h3>
 
 
-// Add user 
+      <h3>Recently Viewed</h3>
 
-
+      <Button>Delete User</Button>
+    </>
+  );
+}
 // Delete User
-
-
 
 //Credit Amount
 
-
-
-// Recent Purchases 
-
-
+// Recent Purchases
 
 // Recently viewed
 
-
-
-
-
-
-
-
-
 // Profile-Profile Picture with upload button,
-function profile(props) {
-  return (
-    <div>
-      <Avatar alt="default avatar" src="/" />
-      <input type="file" />
-      <button>Upload</button>
-    </div>
-  );
-}
 
 export default profile;
