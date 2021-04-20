2567e70b0dde98d49cce2ccc7c2c1fb0910ff391
tuckerserwadda <serwadda.tucker14@gmail.com>
2021-04-20 12:55:38 -0400

    updated all categories to add to cart

--- src/views/Tablets.js
+++ src/views/Tablets.js
@@ -1,18 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { All_Products } from "../FetchRequests";
+import { All_Products,addToCart } from "../FetchRequests";
+import {useStore} from "../store/store"
 
 function Tablet() {
   const [products, setProducts] = useState([]);
   const [tablets, setTablets] = useState([]);
+  const user = useStore((state) =>state.user)
+  const addItemToCart = (event)=>{
+    const productId = (event.target.id)
+    const itemToAdd = products.filter(product =>{
+      return product._id === productId
+    })
+    const item = itemToAdd[0]
+    
+    const productName = item.productName
+    const price = item.price
+    const productDetail = item.productDetail
+    const userId = user.id
+  addToCart(  userId,productName,price, productDetail).
+  then(res => console.log(res))
+
+  }
 
   useEffect(() => {
     All_Products().then((data) => setProducts(data));
+
+  }, []);
+
+  useEffect(() => {
+    
 
     const TabletsCategory = products.filter((products) => {
       return products.category === "tablet";
     });
     setTablets(TabletsCategory);
-  }, [tablets, products]);
+  }, [products]);
   return (
     <div>
       {tablets.map((item) => (
@@ -21,6 +43,13 @@ function Tablet() {
           <>
             <h1> {item.productName} </h1>
             <p> {item.productDetail} </p>
+            <p> {item.price} </p>
+            {user.token &&
+            <div> <button
+            onClick = {addItemToCart}
+            id = {item._id}
+              >Add To Cart</button></div>
+             }
           </>
         </div>
       ))}
