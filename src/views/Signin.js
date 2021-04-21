d151cb02ba66a384287148b2fa8c2cc46a9517e5
petez32 <petezamora32@gmail.com>
2021-04-21 10:38:05 -0500

    Changes to Signin and Signup

--- src/views/Signin.js
+++ src/views/Signin.js
@@ -4,7 +4,7 @@ import { TextField, Button } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
 import { signInRequest } from "../FetchRequests";
 import { useStore, actions } from "../store/store";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 
 function Signin() {
   const history = useHistory();
@@ -14,33 +14,16 @@ function Signin() {
     setUserInput((state) => ({ ...state, [e.target.name]: e.target.value }));
   };
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (userInput.password.length === 0 || userInput.username.length === 0) {
-<<<<<<< HEAD
-      dispatch({
-        type: actions.TOAST,
-        payload: { message: "All fields must be completed", statusCode: 400 },
-      });
+      toast.error("All fields must be completed");
     } else if (userInput.password.length < 4) {
-      dispatch({
-        type: actions.TOAST,
-        payload: {
-          message: "password should be longer than 3 characters",
-          statusCode: 400,
-        },
-      });
+      toast.error("password must be longer than 3 characters");
     } else {
-      console.log({
-        username: userInput.username,
-        password: userInput.password,
+      signInRequest(userInput.username, userInput.password).then((userData) => {
+        dispatch({ type: actions.LOGIN, payload: userData });
+        history.push("/");
       });
-      dispatch({
-        type: actions.TOAST,
-        payload: { message: " Successfully logged in", statusCode: 201 },
-      });
-      signInRequest(userInput.username, userInput.password).then((userData) =>
-        dispatch({ type: actions.LOGIN, payload: userData })
-      );
     }
   };
   return (
@@ -64,66 +47,19 @@ function Signin() {
           type="password"
         />
         <Link to="/">
-          <center>
-            <Button onClick={handleSubmit} variant="contained" color="primary">
-              Submit
-            </Button>
-          </center>
+          <Button onClick={handleSubmit} variant="contained" color="primary">
+            Submit
+          </Button>
         </Link>
         <span>
           {" "}
           <center>
-            Not a member <Link to="/signup">Sign Up</Link> for 50 bonus credits.
+            Not a member <Link to="/signup">Sign Up</Link> for X amount of
+            credits.
           </center>
         </span>
       </form>
     </div>
   );
 }
-
 export default Signin;
-=======
-          toast.error("All fields must be completed")
-    }else if(userInput.password.length < 4) {
-            toast.error("password must be longer than 3 characters")
-    }
-    else{
-      signInRequest(userInput.username, userInput.password)
-      .then((userData) => {
-      dispatch({type:actions.LOGIN,payload:userData})
-      history.push("/")
-    })}}
-    return(
-  
-        <div id="signin-div">
-            <form id="signin-form">
-              <div className="loginText">
-                <h1>Sign-In</h1>
-              </div>
-              <br />
-                <TextField
-                    name="username"
-                    onChange={handleInput}
-                    label="UserName"
-                    variant="filled"
-                />
-                <TextField
-                    onChange={handleInput}
-                    label="Password"
-                    name="password"
-                    variant="filled"
-                    type="password"
-                />
-                <Link to="/" >
-                <Button onClick={handleSubmit} variant="contained" color="primary">Submit</Button>
-                </Link>
-                <span> <center>
-                Not a member <Link to="/signup">Sign Up</Link> for X amount of credits.
-                </center>
-                </span>
-            </form>
-        </div>
-    )}
-  export default Signin;
-  
->>>>>>> main
