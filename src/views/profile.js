3fe6da1168e342d44ea07d11d3752f1e9ab94120
tuckerserwadda <serwadda.tucker14@gmail.com>
2021-04-21 18:13:35 -0400

    profile page update

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,19 +1,34 @@
-import React, { useReducer } from 'react'
+import React, { useEffect,useState} from 'react'
 import { Link, Route } from 'react-router-dom'
 import EditProfile from '../component/EditProfile'
 import {useStore} from "../store/store"
 
 const Profile = () => {
     const user = useStore((state) =>state.user)
+    const [currentUser, setCurrentUser] = useState({})
+ 
+    useEffect(()=>{
+      if(user.loaded === true){
+      fetch( "http://localhost:5000/user/" + user.id ,{
+      method: "GET",
+      headers:{"Content-Type":"application/json"}})
+      .then((response) => response.json())
+  .then (response => setCurrentUser(response))
+      }
+      
+    }, [user, currentUser])
     return (
+     
+    
         <>
         <div>
-          <h1>{(user.userName).toUpperCase()}'s Profile </h1>
+          <h1>{(currentUser &&currentUser.userName)}'s Profile </h1>
+          <h3>Credit Balance: {currentUser.credits}</h3>
           <div>Click To<Link to = "/Profile/edit"><strong>  Edit</strong> Profile </Link>  or  <Link to = "/user/order"> View Your Order</Link></div>
-           
-            <div> Name:<span>{(user.firstName).toUpperCase()}  {(user.lastName).toUpperCase()}</span>  </div>
-            <div> User Name:<span>{user.userName}  </span>  </div>
-            <div> Email:<span>{user.email}  </span>  </div>
+          
+            <div> Name:<span>{(currentUser.firstName)}  {(currentUser.lastName)}</span>  </div>
+            <div> User Name:<span>{currentUser.userName}  </span>  </div>
+            <div> Email:<span>{currentUser.email}  </span>  </div>
         </div>
       
 
